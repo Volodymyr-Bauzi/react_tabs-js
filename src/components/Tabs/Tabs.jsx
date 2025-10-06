5fdbd4a36aad30a2124e04b39da7a0b2fc05915c
Bauzi <volodiasyd@gmail.com>
2025-10-06 16:17:08 +0300

    Solution #4

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,4 +1,5 @@
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  if (!Array.isArray(tabs) || tabs.length === 0) return null;
   const selectedTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
@@ -7,14 +8,17 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
         <ul>
           {tabs.map(tab => (
             <li
-              className={`${tab.id === selectedTab.id && 'is-active'}`}
+              className={`${tab.id === selectedTab.id ? 'is-active' : ''}`}
               data-cy="Tab"
               key={tab.id}
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => tab.id !== activeTabId && onTabSelected(tab.id)}
+                onClick={event => {
+                  event.preventDefault();
+                  if (tab.id !== selectedTab.id) onTabSelected(tab.id);
+                }}
               >
                 {tab.title}
               </a>
